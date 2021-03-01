1eb72503319a40983c631a8dbb12996a1460d8ba
Suada Rantisi <suadarantisi@gmail.com>
2021-03-01 10:38:59 +0700

    Commit ke-7, audio fixed 01-03-21

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-let audioPlaying = true,
-  backgroundAudio,
-  browser;
-browser = navigator.userAgent.toLowerCase();
-$('<audio class="audio1" src="audio.mp3" loop></audio>').prependTo("body");
-if (!browser.indexOf("firefox") > -1) {
-  $('<embed id="background-audio" src="audio.mp3" autostart="1"></embed>').prependTo("body");
-  backgroundAudio = setInterval(function () {
-    $("#background-audio").remove();
-    $('<embed id="background-audio" src="audio.mp3"></embed>').prependTo("body");
-  }, 120000); // 120000 is the duration of your audio which in this case 2 mins.
+document.addEventListener("click", musicPlay);
+function musicPlay() {
+  document.getElementById("myAudio").play();
+  // document.removeEventListener("click", musicPlay);
 }
