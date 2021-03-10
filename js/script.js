d5e12bfb18430071fa0b89378f9a36474bf7ce5e
Suada Rantisi <suadarantisi@gmail.com>
2021-03-10 13:01:20 +0700

    Add logo,dll 10-03-21

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ function musicPlay() {
   document.getElementById("myAudio").play();
   // document.removeEventListener("click", musicPlay);
 }
+
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+});
