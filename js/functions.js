373b685aea425305166871f366e9ae2a723d7525
Syuhada Rantisi <suadarantisi@gmail.com>
2022-02-21 09:46:37 +0700

    Perubahan besar, add JS DOM to show project and achievment - 21-02-22

---
+++ js/functions.js
@@ -0,0 +1,67 @@
+function showProject(data){
+	return `
+          <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem">
+              <img
+                src="img/${data.img}"
+                class="card-img-top"
+                alt="My Project"
+              />
+              <div class="card-body">
+                <h5 class="card-title">${data.title}</h5>
+                <p class="card-text">
+					${data.desc}
+                </p>
+                <a href="${data.url}" class="btn btn-success"
+                  >View project <i class="bi bi-arrow-right-circle-fill"></i
+                ></a>
+                <br /><br />
+                <a
+                  href="${data.urlSc}"
+                  target="_blank"
+                  class="btn btn-primary"
+                  >Source code <i class="bi bi-github"></i
+                ></a>
+              </div>
+            </div>
+          </div>` 
+}
+
+function showAchievment(data){
+	return `
+          <div class="col-md-4">
+            <div class="card" style="width: 20rem">
+              <img
+                src="img/${data.img}"
+                class="card-img-top"
+                alt="Certificate"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  <b>${data.title}</b> 
+				  ${data.desc}
+				  </p>
+                <p>
+                  Sumber :
+                  <a href="${data.url}">${data.sumber}</a>
+                </p>
+              </div>
+            </div>
+          </div>
+		  `
+}
+
+function updateUI(data, className, func){
+	let cards = ''
+	data.forEach(d => {
+		cards += func(d)
+	})
+	const resCon = document.querySelector(`.${className}`)
+	resCon.innerHTML = cards
+}
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ async function handleSubmit(event) {
     },
   })
     .then((response) => {
-      // ketika berhasil tampilkan alert dan ubah btn Loading jadi Submit
+	  // ketika berhasil tampilkan alert dan ubah btn Loading jadi Submit
       myAlert.classList.toggle("alert-warning");
       myAlert.classList.toggle("d-none");
       status.innerHTML = "<b>Thank you!</b> your message was sent successfully";
@@ -55,3 +55,96 @@ async function handleSubmit(event) {
     });
 }
 form.addEventListener("submit", handleSubmit);
+
+
+/* === Show card project === */
+const project = [
+	{
+		title: 'Portfolio',
+		img: 'portfolio.png',
+		desc: 'This is a portfolio website that tells about me',
+		url: '#',
+		urlSc: 'https://github.com/syuhendar729/syuhendar729.github.io',
+	},
+	{
+		title: 'Simple Blog',
+		img: 'html-css.png',
+		desc: 'This is a simple blog that contains only writing with css and html',
+		url: 'http://47.254.194.164/',
+		urlSc: 'https://github.com/syuhendar729/'
+	},
+	{
+		title: 'Simple Crud',
+		img: 'ci4app.png',
+		desc: 'This is my simple CRUD project with the help of CodeIgniter4 framework',
+		url: 'http://47.254.194.164:81/',
+		urlSc: 'https://github.com/syuhendar729/ci4app'
+	},
+	{
+		title: 'System Login 1',
+		img: 'ci4login.png',
+		desc: 'This is a simple login system and forgot password fiture with CodeIgniter4 framework',
+		url: 'https://ci4appsr.epizy.com/',
+		urlSc: 'https://github.com/syuhendar729/ci4login'
+	},
+	{
+		title: 'Simple Login 2',
+		img: 'ci4applogin.png',
+		desc: 'It is a simple login system and add CRUD features with the help of the codeigniter4 framework',
+		url: 'http://47.254.194.164:83/',
+		urlSc: 'https://github.com/syuhendar729/ci4applogin'
+	},
+	{
+		title: 'My Blog',
+		img: 'blog.png',
+		desc: `It's a my blog. Build with nextjs and tailwind. Hosting in <a href="https://vercel.com">Vercel</a>`,
+		url: 'https://syuhendar729.vercel.app/',
+		urlSc: 'https://github.com/syuhendar729/nextjs-myblog'
+	}
+]
+
+const achievment = [
+	{
+		img: 'cer-node.png',
+		title: 'NodeJS',
+		desc: 'adalah platform perangkat lunak pada sisi peladen dan aplikasi jaringan. Ditulis dengan bahasa JavaScript dan dijalankan pada Windows, Mac OS X, dan Linux tanpa perubahan kode program.',
+		url: 'https://id.wikipedia.org/wiki/Node.js',
+		sumber: 'Wikipedia'
+	},
+	{
+		img: 'cer-py.png',
+		title: 'Python',
+		desc: 'adalah bahasa pemrograman tujuan umum yang ditafsirkan, tingkat tinggi. Dibuat oleh Guido van Rossum dan pertama kali dirilis pada tahun 1991, filosofi desain Python menekankan keterbacaan kode dengan penggunaan spasi putih yang signifikan.',
+		url: 'https://id.wikipedia.org/wiki/Python_(bahasa_pemrograman)',
+		sumber: 'Wikipedia'
+	},
+	{
+		img: 'cer-AC.png',
+		title: 'Cloud Server',
+		desc: 'adalah gabungan pemanfaatan teknologi komputer dan pengembangan berbasis Internet. Awan adalah metafora dari internet, sebagaimana awan yang sering digambarkan di diagram jaringan komputer.',
+		url: 'https://id.wikipedia.org/wiki/Komputasi_awan',
+		sumber: 'Wikipedia'
+	},
+	{
+		img: 'cer-rc.png',
+		title: 'Recloud',
+		desc: 'adalah kompetisi dengan sekumpulan tantangan untuk ragam layanan awan dan sertifikasi yang disediakan oleh Alibaba Cloud. Acara ini bisa diikuti oleh seluruh warga Indonesia dari mulai professional hingga mahasiswa untuk turut berkompetisi.' ,
+		url: 'https://re-cloud.id/',
+		sumber: 'Re-cloud.id'
+	}
+]
+
+updateUI(project, 'project-content', showProject)
+updateUI(achievment, 'achievment-content', showAchievment)
+
+
+
+
+
+
+
+
+
+
+
+
